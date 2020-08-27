4577cc1a0adae48a9fd96700d6aeef229d7e82ac
Fernando Silva <fernnlcs@gmail.com>
2020-08-27 01:53:21 -0300

    Create r5.cpp

---
+++ Lab20/r5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 0;
+	cout << "Digite um número: ";
+	if (cin >> x; x > 0)
+		cout << "Número positivo!" << endl;
+	else
+		cout << "Número negativo!" << endl;
+}
