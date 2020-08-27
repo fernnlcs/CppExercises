6022940014a5ef5b341826e59619466f30f5d658
Fernando Silva <fernnlcs@gmail.com>
2020-08-27 01:50:11 -0300

    Create f1.cpp

---
+++ Lab06/f1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int total = 0;
+	cout << "Inicialmente total = " << total << " (ZERO)" << endl;
+	
+	total = 50;
+	cout << "Depois da atribuicao total = " << total << endl;
+
+	return 0;
+}
