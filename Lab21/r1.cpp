f95b956aac246fd0adb2e03f141a9aa19b628de4
Fernando Silva <fernnlcs@gmail.com>
2020-08-27 01:53:27 -0300

    Create r1.cpp

---
+++ Lab21/r1.cpp
@@ -0,0 +1,11 @@
+/*
+RESPOSTAS:
+
+a) if (peso >= 115 && peso < 125)
+b) if (ch == 'q' || ch == 'Q')
+c) if (x % 2 == 0 || x != 26)
+d) if (x % 2 == 0 || x % 26 != 0)
+e) if ((donativo >= 10 && donativo <= 20) || (donativo >= 90 && donativo <= 100))
+f) if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+
+*/
