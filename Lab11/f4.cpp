f6834bb554b3dd5bc7bc0260a833f40ed1dbf360
Fernando Silva <fernnlcs@gmail.com>
2020-08-27 01:51:06 -0300

    Create f4.cpp

---
+++ Lab11/f4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char nome[] = "C++ Primer Plus";
+
+	cout << strlen(nome) << " caracteres." << endl << sizeof(nome) << " bytes usados.";
+
+	return 0;
+}
